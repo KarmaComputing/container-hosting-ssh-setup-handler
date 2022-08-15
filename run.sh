f62d47c375e551cd04e961bf8983967d904985f7
chrisjsimpson <chris.j.simpson@live.co.uk>
2022-08-15 21:43:37 +0100

    FLASK_ENV is deprecated. Updated to FLASK_DEBUG

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 
 # Note must be ran as dokku user or a user with (sudoers) permissions to edit
 # the file /home/dokku/.ssh/authorized_keys
-FLASK_ENV=development flask run --host 0.0.0.0
+FLASK_DEBUG=1 flask run --host 0.0.0.0
