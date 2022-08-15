b5084028c881f498266b879b66387146177300de
chrisjsimpson <chris.j.simpson@live.co.uk>
2022-08-15 21:34:09 +0100

    basic api auth and error handling

--- README.md
+++ README.md
@@ -3,3 +3,8 @@
 Recieve a POST request public key and append to authorized_keys file.
 
 Main project: https://github.com/KarmaComputing/container-hosting
+
+## Test
+```
+curl http://127.0.0.1:5000 -H 'Content-Type: application/json' -d '{"CONTAINER_HOSTING_SSH_SETUP_HANDLER_API_KEY": "secret", "public_key": "ssh-rsa AAAAB3Nza..."}'
+```
--- app.py
+++ app.py
@@ -1,15 +1,41 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 import json
+import os
+import logging
+
+# This is likely not thread safe.
+
+CONTAINER_HOSTING_SSH_SETUP_HANDLER_API_KEY = os.getenv("CONTAINER_HOSTING_SSH_SETUP_HANDLER_API_KEY", None)
+AUTHORIZED_KEYS_PATH = os.getenv("AUTHORIZED_KEYS_PATH", None)
+
+if CONTAINER_HOSTING_SSH_SETUP_HANDLER_API_KEY is None:
+    exit("CONTAINER_HOSTING_SSH_SETUP_HANDLER_API_KEY must be set")
+
+if AUTHORIZED_KEYS_PATH is None:
+    exit("AUTHORIZED_KEYS_PATH must be set")
 
 app = Flask(__name__)
 
 @app.route("/", methods=["GET", "POST"])
 def index():
-    public_key = json.loads(request.data.decode("utf-8"))['public_key']
+    payload = json.loads(request.data.decode("utf-8"))
+    api_key = payload['CONTAINER_HOSTING_SSH_SETUP_HANDLER_API_KEY']
+    if api_key != CONTAINER_HOSTING_SSH_SETUP_HANDLER_API_KEY:
+        msg = "Invalid api key sent for CONTAINER_HOSTING_SSH_SETUP_HANDLER_API_KEY"
+        logging.warning(msg)
+        return jsonify(msg), 401
+
+    public_key = payload['public_key']
     # Append write public_key to authorized_keys
-    with open("/home/dokku/.ssh/authorized_keys", "a") as fp:
-        fp.write(f"\n{public_key}")
-        print(f"Appended public_key {public_key}")
-    print(public_key)
-    print(request)
-    return "Thanks- got the public_key, and appended it to authorized_keys, probably!"
+    try:
+        with open(AUTHORIZED_KEYS_PATH, "a") as fp:
+            fp.write(f"\n{public_key}")
+            print(f"Appended public_key {public_key}")
+        logging.info(public_key)
+        return jsonify("Thanks- got the public_key, and appended it to authorized_keys, probably!"), 200
+    except FileNotFoundError:
+        logging.error(f"FileNotFoundError for {AUTHORIZED_KEYS_PATH}")
+        return jsonify("Unable to append key. See logs"), 500
+    except Exception as e:
+        logging.error(f"Unhandled exception: {e}")
+        return jsonify("Unable to append key. See logs"), 500
