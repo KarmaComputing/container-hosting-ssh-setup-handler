a6a783ee739f068526766af8164f4da0c751a77a
chrisjsimpson <chris.j.simpson@live.co.uk>
2022-08-20 13:58:43 +0000

    add auth wrapper api_key_required, add route /CONTAINER_HOSTING_API_KEY which stores api key per container APP_NAME

--- app.py
+++ app.py
@@ -2,11 +2,14 @@ from flask import Flask, request, jsonify
 import json
 import os
 import logging
+import functools
+import sqlite3
 
 # This is likely not thread safe.
 
 CONTAINER_HOSTING_SSH_SETUP_HANDLER_API_KEY = os.getenv("CONTAINER_HOSTING_SSH_SETUP_HANDLER_API_KEY", None)
 AUTHORIZED_KEYS_PATH = os.getenv("AUTHORIZED_KEYS_PATH", None)
+APPS_DB_FULL_PATH = os.getenv("APPS_DB_FULL_PATH", None)
 
 if CONTAINER_HOSTING_SSH_SETUP_HANDLER_API_KEY is None:
     exit("CONTAINER_HOSTING_SSH_SETUP_HANDLER_API_KEY must be set")
@@ -14,16 +17,32 @@ if CONTAINER_HOSTING_SSH_SETUP_HANDLER_API_KEY is None:
 if AUTHORIZED_KEYS_PATH is None:
     exit("AUTHORIZED_KEYS_PATH must be set")
 
+if APPS_DB_FULL_PATH is None:
+    exit("APPS_DB_FULL_PATH must be set")
+
 app = Flask(__name__)
 
+def api_key_required(view):
+    @functools.wraps(view)
+    def wrapped_view(**kwargs):
+        try:
+            api_key = request.json['api_key']
+            if api_key != CONTAINER_HOSTING_SSH_SETUP_HANDLER_API_KEY:
+                msg = "Invalid api key sent for CONTAINER_HOSTING_SSH_SETUP_HANDLER_API_KEY"
+                logging.warning(msg)
+                return jsonify(msg), 401
+        except Exception as e:
+           msg = f"Failed authentication {e}"
+           logging.error(msg)
+           return jsonify("Failed authentication"), 401
+        return view(**kwargs)
+
+    return wrapped_view
+
 @app.route("/", methods=["GET", "POST"])
+@api_key_required
 def index():
     payload = json.loads(request.data.decode("utf-8"))
-    api_key = payload['CONTAINER_HOSTING_SSH_SETUP_HANDLER_API_KEY']
-    if api_key != CONTAINER_HOSTING_SSH_SETUP_HANDLER_API_KEY:
-        msg = "Invalid api key sent for CONTAINER_HOSTING_SSH_SETUP_HANDLER_API_KEY"
-        logging.warning(msg)
-        return jsonify(msg), 401
 
     public_key = payload['public_key']
     # Append write public_key to authorized_keys
@@ -39,3 +58,21 @@ def index():
     except Exception as e:
         logging.error(f"Unhandled exception: {e}")
         return jsonify("Unable to append key. See logs"), 500
+
+
+@app.route("/CONTAINER_HOSTING_API_KEY", methods=["POST"])
+@api_key_required
+def store_CONTAINER_HOSTING_API_KEY():
+    """Store users app CONTAINER_HOSTING_API_KEY
+    each APP_NAME has its own CONTAINER_HOSTING_API_KEY"""
+    data = request.json
+    APP_NAME = data["APP_NAME"]
+    CONTAINER_HOSTING_API_KEY = data["CONTAINER_HOSTING_API_KEY"]
+    # Store APP_NAME and its CONTAINER_HOSTING_API_KEY
+    con = sqlite3.connect(APPS_DB_FULL_PATH)
+    cur = con.cursor()
+    # CREATE TABLE container (container TEXT NOT NULL, CONTAINER_HOSTING_API_KEY TEXT);
+    query = "INSERT INTO container (container, CONTAINER_HOSTING_API_KEY) VALUES (?, ?)"
+    cur.execute(query, (APP_NAME, CONTAINER_HOSTING_API_KEY))
+    con.commit()
+    return CONTAINER_HOSTING_API_KEY
